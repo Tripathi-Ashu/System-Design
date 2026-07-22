9d6cc58359cd23e6ad5ef37b73d5ddd48ea47113
Ashutosh tripathi <151380927+Tripathi-Ashu@users.noreply.github.com>
2026-07-22 09:39:26 +0000

    new think

--- oops/StaticDataMember.cpp
+++ oops/StaticDataMember.cpp
@@ -1,36 +1,41 @@
-// They are attribute of clases or class member 
-// it is declear using static keyword 
-// Only one copy of the member is created for the entiry 
-// class and use all object 
-
- 
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-
-class Customer 
-{
-   string name;
-   int acc_number , balance;
-   int total;
-
-   public:
-
-   Customer (string name, int acc_number , int balance)
-   {
-     this->name= name;
-     this->acc_number = acc_number;
-     this->balance = balance;
-     total = 1;
-   }
-
-   void display (){
-    cout<<name<< " " << acc_number<< " " << balance << " " << total;
+class Customer {
+    string name;
+    int acc_number;
+    int balance;
     
-   }
+public:
+    // 1. Declare static variable inside the class
+    static int total;
+
+    Customer(string name, int acc_number, int balance) {
+        this->name = name;
+        this->acc_number = acc_number;
+        this->balance = balance;
+        
+        // 2. Increment total for each new customer created
+        total++; 
+    }
+
+    void display() {
+        cout << name << " " << acc_number << " " << balance << " " << total << endl;
+    }
+
+    void display_total() {
+        cout << total << endl;
+    }
 };
 
-int main(){
-    Customer A1("Ashutosh" , 1 , 1000);
-    Customer A2("Ashsu" , 2 , 20000);
+// 3. Define and initialize the static member outside the class
+int Customer::total = 0;
+
+int main() {
+    Customer A1("Ashutosh", 1, 1000);
+    Customer A2("Mohit" , 2, 8000);
+    A1.display();
+    A2.display_total();
+    return 0;
 }
