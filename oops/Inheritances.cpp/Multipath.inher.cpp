f11116346f25e233ceb1b63dfdbe5cfe3994f0df
Ashutosh tripathi <151380927+Tripathi-Ashu@users.noreply.github.com>
2026-07-28 04:46:55 +0000

    MultiPath is completed;

---
+++ oops/Inheritances.cpp/Multipath.inher.cpp
@@ -0,0 +1,39 @@
+// Dimond Problem 
+  
+#include <iostream>
+using namespace std;
+
+
+class ClassA 
+{
+    public:
+    int a;
+};
+
+class ClassB: virtual public ClassA 
+{
+    public:
+    int b;
+};
+
+class ClassC: virtual public ClassA
+{
+    public:
+    int c;
+};
+
+class ClassD : public ClassB , public ClassC {
+    public:
+    int d;
+};
+
+int main()
+{
+     ClassD obj;
+     obj.a = 100;
+
+
+     cout<< "Vlaue of a: " << obj.a << endl;
+
+     return 0;
+}
