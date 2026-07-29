7f626f332bc4d68d1920bec33106e83464a90b2e
Ashutosh tripathi <151380927+Tripathi-Ashu@users.noreply.github.com>
2026-07-29 11:12:55 +0000

    Polymorphisum

---
+++ oops/Inheritances.cpp/polymorphisium.cpp
@@ -0,0 +1,14 @@
+// polmorphism  -> compile time error , run time error ;
+// function  overloading  , operator overloading;
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() 
+{
+   
+};
+
